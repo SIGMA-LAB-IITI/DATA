7ec804ffa49eae97a0297ba0f0b50462d2afdb8e
SIGMA-LAB-IITI <142662669+SIGMA-LAB-IITI@users.noreply.github.com>
2023-09-27 20:33:23 +0530

    Update 2_LIDAR Ceilometer.py

--- pages/2_LIDAR Ceilometer.py
+++ pages/2_LIDAR Ceilometer.py
@@ -8,7 +8,7 @@ with col1:
     st.image("./LIDAR Ceilometer (BL View)/CM.jpeg")
     
 with col2:
-    st.image("/Users/snande/Personal/Projects/dataDisplay/LIDAR Ceilometer (BL View)/PXL_20230310_080803224.jpeg")
+    st.image("./LIDAR Ceilometer (BL View)/PXL_20230310_080803224.jpeg")
     
 with col3:
     st.write("""Ceilometer is a remote sensing instrument used for measuring the height and extent of clouds and aerosols in the atmosphere. It operates using the principle of light detection and ranging (LIDAR), which involves emitting pulses of laser light and measuring the time it takes for the light to reflect back from atmospheric particles.
