98793f3ae20edb28c34bc9716edd4af8735c4bbe
SIGMA-LAB-IITI <142662669+SIGMA-LAB-IITI@users.noreply.github.com>
2023-09-27 20:47:36 +0530

    Update SIGMA.py

--- SIGMA.py
+++ SIGMA.py
@@ -16,6 +16,7 @@ st.markdown(
 st.sidebar.image("Indian_Institute_of_Technology,_Indore_Logo.png", width=150)  # Replace "logo.png" with the path to your logo image
 
 st.title("SIGMA Research Lab")
+st.subtitle("DAASE, IIT Indore")
 st.write("Space Science ||  Climate Informatics  ||   GPS/GNSS  ||   Radar Meteorology ||  Atmospheric Remote Sensing")
 
 st.write("""Welcome to SIGMA Research Lab, Department of Astronomy, Astrophysics and Space Engineering, Indian Institute of Technology Indore, a dynamic and innovative research group dedicated to advancing knowledge and innovation in Radiowave Propagation and Satellite Communication, Microwave Remote Sensing, Radar Meteorology, Satellite-based Navigation, Space Weather and Ionosphere, Climate Change and Tropical Rain, and Machine Learning and Data Science in Atmospheric and Space Science Problems. At SIGMA Research Lab, our team of researchers, students are passionate about exploring new frontiers in these exciting and rapidly evolving fields. We are committed to conducting cutting-edge research that addresses some of the most pressing challenges facing our planet and contributes to the sustainable development of our society. For more details on the current research by the group members, click here.""")
