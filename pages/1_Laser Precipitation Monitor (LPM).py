ec55c46647dc827bb4c37aed3a398218ab5ffc54
SIGMA-LAB-IITI <142662669+SIGMA-LAB-IITI@users.noreply.github.com>
2023-09-27 20:26:13 +0530

    Update 1_Laser Precipitation Monitor (LPM).py

--- pages/1_Laser Precipitation Monitor (LPM).py
+++ pages/1_Laser Precipitation Monitor (LPM).py
@@ -5,7 +5,7 @@ st.header("Laser Precipitation Monitor (LPM)")
 col1, col2 = st.columns([1,2])
 
 with col1:
-    st.image("/Users/snande/Personal/Projects/dataDisplay/Laser Precipitation Monitor (LPM)/LPMImage.jpeg")
+    st.image("Laser Precipitation Monitor (LPM)/LPMImage.jpeg")
     
 with col2:
     st.write("""Laser Precipitation Monitor (LPM) with a wind sensor is an advanced instrument that is used to measure and monitor precipitation. It works on the principle of laser scattering, which is used to detect the presence of precipitation particles in the atmosphere. The wind sensor measures the speed and direction of the wind, which is necessary for the accurate measurement of precipitation. 
